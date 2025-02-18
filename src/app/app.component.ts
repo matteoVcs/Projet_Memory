32ea1052b26586b0eb042200ecd2fc6b4fb3e012
Dimitri <dimitri.brancourt@gmail.com>
2025-02-18 14:20:03 +0100

    clean router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project_memory';
+  title = 'Application Memory';
 }
